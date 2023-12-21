27314684d306d57101d6bbf96eee145d184a8d67
vishesh0003 <113722235+vishesh0003@users.noreply.github.com>
2023-12-21 10:19:05 +0000

    y

---
+++ javatest/dataTypes.js
@@ -0,0 +1,19 @@
+// "use strict"; treat all JS code as newer version//
+
+// alert("3+3")  We are using node.js, not a browser//
+// console.log("hire");
+
+// some data types to remember
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null = standalone value
+// undefined =>
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // null
--- javatest/variables.js
+++ javatest/variables.js
@@ -2,8 +2,15 @@ const accountId = 14453
 let accountEmail = "visheshnarwal32@gmail.com"
 var accountPassword = "12345"
 accountCity = "Haryana"
+let accountState;
 
-accountId = 2
-
+// accountId = 2 not allowed
+accountEmail = "hcc@gmail.com"
+accountPassword = "24411"
 
 console.log(accountId);
+
+/* Prefer not to use var because of issue in block scope and functional scope 
+*/
+
+console.table([accountId, accountPassword, accountCity,accountState])
