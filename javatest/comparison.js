200d038daf48a0a55aaf1cb9a6278e84b2d99189
vishesh0003 <113722235+vishesh0003@users.noreply.github.com>
2023-12-22 09:15:20 +0000

    y

---
+++ javatest/comparison.js
@@ -0,0 +1,9 @@
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null > 0);
+
+//  == and <= works differently in javascript
+
+// === not only compare them but also check the form of the datatype :)
+
+console.log("2" === 2);
