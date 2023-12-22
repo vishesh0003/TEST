9b64b80dd166f73aa0274c20ba746706a34b6d6a
vishesh0003 <113722235+vishesh0003@users.noreply.github.com>
2023-12-22 08:16:36 +0000

    d

---
+++ javatest/coversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score))
+
+let valueInnumber = Number(score);
+console.log(typeof valueInnumber);
+
+console.log(valueInnumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = 1
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true 0 => false
+// "" => false
+// "Vishesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
